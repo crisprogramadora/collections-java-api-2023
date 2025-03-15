579522441c30c03c9a6a90525674d801a97b1195
Cristiane Sá <crisprogramadora24@outlook.com>
2025-03-15 18:29:26 -0300

    Exercícios lista
    Signed-off-by: Cristiane Sá <crisprogramadora24@outlook.com>

--- src/main/java/list/Ordenacao/OrdenacaoPessoas.java
+++ src/main/java/list/Ordenacao/OrdenacaoPessoas.java
@@ -1,4 +1,47 @@
 package main.java.list.Ordenacao;
 
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.List;
+
 public class OrdenacaoPessoas {
+    private List<Pessoa> pessoaList;
+
+    public OrdenacaoPessoas() {
+        this.pessoaList = new ArrayList<>();
+    }
+
+    public void adicionarPessoa(String nome, Integer idade, Double altura) {
+        pessoaList.add(new Pessoa(nome, idade, altura));
+    }
+
+    public List<Pessoa> ordenarPorIdade() {
+        List<Pessoa> pessoasPorIdade = new ArrayList<>(pessoaList);
+        Collections.sort(pessoasPorIdade);
+        return pessoasPorIdade;
+    }
+
+    public List<Pessoa> ordenarPorAltura() {
+        List<Pessoa> pessoaPorAltura = new ArrayList<>(pessoaList);
+        Collections.sort(pessoaPorAltura, new ComparatorPorAltura());
+        return pessoaPorAltura;
+    }
+
+    public static void main(String[] args) {
+        OrdenacaoPessoas ordenacaoPessoas = new OrdenacaoPessoas();
+
+        ordenacaoPessoas.adicionarPessoa("Kathleen", 14, 1.65);
+        ordenacaoPessoas.adicionarPessoa("Ana", 15, 1.62);
+        ordenacaoPessoas.adicionarPessoa("Cris", 38, 1.60);
+        ordenacaoPessoas.adicionarPessoa("Anderson", 43, 1.75);
+
+        System.out.println(ordenacaoPessoas.pessoaList);
+        System.out.println(ordenacaoPessoas.ordenarPorIdade());
+        System.out.println(ordenacaoPessoas.ordenarPorAltura());
+
+
+
+    }
+
 }
+
---
+++ src/main/java/set/Pesquisa/AgendaContatos.java
@@ -0,0 +1,25 @@
+package main.java.set;
+
+import java.util.HashSet;
+import java.util.Set;
+
+public class AgendaContatos {
+    private Set<Contato> contatoSet;
+
+    public AgendaContatos() {
+        this.contatoSet = new HashSet<>();
+    }
+
+    public void adicionarContato(String nome, int numero) {
+        contatoSet.add(new Contato(nome, numero));
+    }
+
+    public void exibirContatos() {
+        System.out.println(contatoSet);
+    }
+
+    public String pesquisarPorNome(String nome) {
+        
+    }
+    atualizarNumeroContato(String nome, int novoNumero)
+}
