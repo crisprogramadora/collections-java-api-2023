8bb5a19452e7a3d5d8d20454e9316f61e65623fd
Cristiane Sá <crisprogramadora24@outlook.com>
2025-03-15 18:32:11 -0300

    Exercícios Map
    Signed-off-by: Cristiane Sá <crisprogramadora24@outlook.com>

--- src/main/java/map/OperacoesBasicas/AgendaContatos.java
+++ src/main/java/map/OperacoesBasicas/AgendaContatos.java
@@ -1,4 +1,54 @@
 package main.java.map.OperacoesBasicas;
 
+import java.util.HashMap;
+import java.util.Map;
+
 public class AgendaContatos {
+    private Map<String, Integer> agendaContatoMap;
+
+    public AgendaContatos() {
+        this.agendaContatoMap = new HashMap<>();
+    }
+
+    public void adicionarContato(String nome, Integer telefone) {
+        agendaContatoMap.put(nome, telefone);
+    }
+
+    public void removerContato(String nome) {
+        if(!agendaContatoMap.isEmpty()) {
+            agendaContatoMap.remove(nome);
+        }
+    }
+
+    public void exibirContatos() {
+        System.out.println(agendaContatoMap);
+    }
+
+    public Integer pesquisarPorNome(String nome) {
+        Integer numeroPorNome = null;
+        if(!agendaContatoMap.isEmpty()) {
+            numeroPorNome = agendaContatoMap.get(nome);
+        }
+        return numeroPorNome;
+    }
+
+    public static void main(String[] args) {
+        AgendaContatos agendaContatos = new AgendaContatos();
+
+        //se tentar adicionar mais de um contato com o mesmo nome, só vai adicionar, o métod put adiciona e atualiza, portanto, vai aparecer o que foi adicionado por último.
+
+        agendaContatos.adicionarContato("Cristiane", 947586950);
+        agendaContatos.adicionarContato("Anderson", 629049585);
+        agendaContatos.adicionarContato("Kathleen", 98958575);
+        agendaContatos.adicionarContato("Ana", 995659580);
+
+        agendaContatos.exibirContatos();
+
+        agendaContatos.removerContato("Cristiane");
+
+        agendaContatos.exibirContatos();
+
+        System.out.println("O número é: " + agendaContatos.pesquisarPorNome("Ana"));
+
+    }
 }
--- src/main/java/map/Pesquisa/EstoqueProdutos.java
+++ src/main/java/map/Pesquisa/EstoqueProdutos.java
@@ -1,4 +1,74 @@
 package main.java.map.Pesquisa;
 
+import java.util.HashMap;
+import java.util.Map;
+
 public class EstoqueProdutos {
+    private Map<Long, Produto> estoqueProdutosMap;
+
+    public EstoqueProdutos() {
+        this.estoqueProdutosMap = new HashMap<>();
+    }
+
+    public void adicionarProduto(long cod, String nome, int quantidade, double preco) {
+        estoqueProdutosMap.put(cod, new Produto(nome, preco, quantidade));
+    }
+
+    public void exibirProdutos() {
+        System.out.println(estoqueProdutosMap);
+    }
+
+    public double calcularValorTotalEstoque() {
+        double valorTotalEstoque = 0d;
+        if (!estoqueProdutosMap.isEmpty()) {
+            for (Produto p : estoqueProdutosMap.values()) {
+                valorTotalEstoque += p.getQuantidade() * p.getPreco();
+            }
+        }
+        return valorTotalEstoque;
+    }
+
+    public Produto obterProdutoMaisCaro() {
+        Produto produtoMaisCaro = null;
+        double maiorPreco = Double.MIN_VALUE;
+        if (!estoqueProdutosMap.isEmpty()) {
+            for (Produto p : estoqueProdutosMap.values()) {
+                if (p.getPreco() > maiorPreco) {
+                    produtoMaisCaro = p;
+                }
+            }
+        }
+        return produtoMaisCaro;
+    }
+
+    public Produto obterProdutoMaisBarato() {
+        Produto produtoMaisBarato = null;
+        double menorPreco = Double.MAX_VALUE;
+        if (!estoqueProdutosMap.isEmpty()) {
+            for (Produto p : estoqueProdutosMap.values()) {
+                if (p.getPreco() < menorPreco) {
+                    menorPreco = p.getPreco();
+                    produtoMaisBarato = p;
+                }
+            }
+        }
+        return produtoMaisBarato;
+    }
+
+    public static void main(String[] args) {
+        EstoqueProdutos estoque = new EstoqueProdutos();
+//        estoque.exibirProdutos();
+
+        estoque.adicionarProduto(1L, "Tapete", 15, 10.0);
+        estoque.adicionarProduto(2L, "Caderno", 30, 8.0);
+        estoque.adicionarProduto(3L, "Sofá", 5, 1000.0);
+        estoque.adicionarProduto(4L, "Ventilador", 20, 80.0);
+
+        estoque.exibirProdutos();
+
+        System.out.println("Valor total do estoque: R$" + estoque.calcularValorTotalEstoque());
+        System.out.println("Produto mais barato: " + estoque.obterProdutoMaisBarato());
+        System.out.println("Produto mais caro: " + estoque.obterProdutoMaisCaro());
+
+    }
 }
